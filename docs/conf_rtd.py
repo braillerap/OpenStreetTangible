b1845e6b65b0d7c91641f3cbc47f73edd8fc9980
Stephane Godin <pub.sg@free.fr>
2025-10-27 22:03:03 +0100

    remove double config

--- docs/conf_rtd.py
+++ docs/conf_rtd.py
@@ -154,10 +154,10 @@ html_theme = sphinx_html_theme
 # documentation.
 #
 # html_theme_options = {}
-html_theme_options = {
-    'logo_only': False,
-    'display_version': True,
-}
+#html_theme_options = {
+#    'logo_only': False,
+#    'display_version': True,
+#}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
